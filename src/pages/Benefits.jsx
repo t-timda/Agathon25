2795f34c296f181e6f58b2489a118512eb2a5884
이윤서 <yyoo04@naver.com>
2025-07-24 18:07:47 +0900

    혜택 선택

--- src/pages/Benefits.jsx
+++ src/pages/Benefits.jsx
@@ -1,5 +1,33 @@
+import Button from "../components/Button";
+
 const Benefits = () => {
-  return <div>혜택 페이지입니다.</div>;
+  return (
+    <>
+      <div className="flex justify-between ">
+        <div className="text-[36px] font-bold"></div>
+        <Step step="혜택선택" />
+      </div>
+
+      <div className="grid grid-cols-2 grid-rows-2 gap-[20px]">
+        <div className="bg-white rounded-2xl shadow-md">
+          1
+          <Button text={"버튼 신청하기"} />
+        </div>
+        <div className="bg-white rounded-2xl shadow-md">
+          2
+          <Button text={"버튼 신청하기"} />
+        </div>
+        <div className="bg-white rounded-2xl shadow-md">
+          3
+          <Button text={"버튼 신청하기"} />
+        </div>
+        <div className="bg-white rounded-2xl shadow-md">
+          4
+          <Button text={"버튼 신청하기"} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Benefits;
