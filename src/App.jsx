b201342ac51fd59390e8ab94e26921b17a26c7a6
이윤서 <yyoo04@naver.com>
2025-07-24 16:40:39 +0900

    버튼 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import Layout from "./components/Layout.jsx";
 import Login from "./pages/Login.jsx";
 import Main from "./pages/Main.jsx";
 import SelectProperty from "./pages/SelectProperty.jsx";
-import Introduction from './pages/Introduction.jsx'
-import Announcement from './pages/Announcement.jsx'
-import MapoNews from './pages/MapoNews.jsx'
-import MyPage from './pages/MyPage.jsx'
+import Introduction from "./pages/Introduction.jsx";
+import Announcement from "./pages/Announcement.jsx";
+import MapoNews from "./pages/MapoNews.jsx";
+import MyPage from "./pages/MyPage.jsx";
+import PropertyDetail from "./pages/PropertyDetail.jsx";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/Announcement" element={<Announcement />} />
         <Route path="/MapoNews" element={<MapoNews />} />
         <Route path="/MyPage" element={<MyPage />} />
+        <Route path="/propertydeatil" element={<PropertyDetail />} />
       </Route>
     </Routes>
   );
