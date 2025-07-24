68a0a62c5b9d4fc6969bb5a9d90b45daf5ddc681
이정인 <jung020623@g.hongik.ac.kr>
2025-07-24 12:52:12 +0900

    브랜치 이동을 위한 임시저장

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,18 @@
+import React from 'react';
+
 export default function Login() {
-    return (<div>Login페이지입니다</div>);
-};
+  return(<main className="flex flex-col items-center justify-center h-full flex-1 bg-white">
+    <h1 className="text-[#191919] text-2xl font-semibold text-center mb-8">
+      카카오톡으로 간편하게 로그인하고 <br />
+      서비스를 이용해보세요!
+    </h1>
+
+    <button
+      onClick={handleLogin}
+      className="flex items-center justify-center gap-2 w-[280px] py-3 cursor-pointer rounded-md bg-[#FEE500] text-[#191919] hover:opacity-90 transition"
+    >
+      <img src={kakaoLogo} alt="kakao" className="w-6 h-6" />
+      <span className="text-base font-semibold">카카오로 계속하기</span>
+    </button>
+  </main>)
+}
