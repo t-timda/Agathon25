eb1c215c7898c9362ae239b9ab8599eaea4a77ec
이정인 <jung020623@g.hongik.ac.kr>
2025-07-24 12:36:56 +0900

    Chore : 기본 세팅

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = () => {
   return (
     <>
       <Navbar />
-      <div className="pt-20 bg-black text-white min-h-screen">
+      <div className="pt-20 bg-white text-black min-h-screen">
         <Outlet />
       </div>
     </>
