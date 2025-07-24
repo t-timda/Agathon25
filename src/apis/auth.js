07fbaa6070d1a9482a4c384cf0ea640a15776e24
이정인 <jung020623@g.hongik.ac.kr>
2025-07-24 18:40:18 +0900

    fix : 카카오로그인 로컬 확인

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,12 +1,12 @@
 import instance from './axios';
 
 export const login = async (code) => {
-  const res = await instance.post('/login/oauth2/code/kakao', { code });
+  const res = await instance.post('/auth/kakao', { code });
 
   return res.data;
 };
 
 export const getCurrentUser = async () => {
-  const res = await instance.get('/api/user/me');
+  const res = await instance.get('/api/v1/auth/me');
   return res.data;
 };
