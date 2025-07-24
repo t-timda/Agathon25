7c37f3a66dde68305e0efee3fa9a2df8a78855b1
이윤서 <yyoo04@naver.com>
2025-07-24 18:20:54 +0900

    혜택 선택 수정

--- src/pages/Benefits.jsx
+++ src/pages/Benefits.jsx
@@ -1,29 +1,30 @@
 import Button from "../components/Button";
+import Step from "../components/Step";
 
 const Benefits = () => {
   return (
     <>
-      <div className="flex justify-between ">
+      <div className="flex justify-between">
         <div className="text-[36px] font-bold"></div>
         <Step step="혜택선택" />
       </div>
 
-      <div className="grid grid-cols-2 grid-rows-2 gap-[20px]">
-        <div className="bg-white rounded-2xl shadow-md">
+      <div className="grid grid-cols-2 grid-rows-2 gap-x-6 gap-y-6 mt-6 max-w-[1000px] mx-auto">
+        <div className="bg-white rounded-2xl shadow-md w-[480px] h-[240px] flex flex-col items-center content-center justify-between pb-[20px]">
           1
-          <Button text={"버튼 신청하기"} />
+          <Button text="혜택 신청하기" />
         </div>
-        <div className="bg-white rounded-2xl shadow-md">
+        <div className="bg-white rounded-2xl shadow-md w-[480px] h-[240px] flex flex-col items-center content-center justify-between pb-[20px]">
           2
-          <Button text={"버튼 신청하기"} />
+          <Button text="혜택 신청하기" />
         </div>
-        <div className="bg-white rounded-2xl shadow-md">
+        <div className="bg-white rounded-2xl shadow-md w-[480px] h-[240px] flex flex-col items-center content-center justify-between pb-[20px]">
           3
-          <Button text={"버튼 신청하기"} />
+          <Button text="혜택 신청하기" />
         </div>
-        <div className="bg-white rounded-2xl shadow-md">
+        <div className="bg-white rounded-2xl shadow-md w-[480px] h-[240px] flex flex-col items-center content-center justify-between pb-[20px]">
           4
-          <Button text={"버튼 신청하기"} />
+          <Button text="혜택 신청하기" />
         </div>
       </div>
     </>
