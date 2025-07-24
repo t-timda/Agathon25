2f31666c20cc5a06b21c23fc445c6a19eed5d2a6
이윤서 <yyoo04@naver.com>
2025-07-24 15:04:15 +0900

    scroll card component

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,7 +1,7 @@
-const PropertyCard = (name) => {
+const PropertyCard = ({ name }) => {
   return (
     <>
-      <div className="bg-white border-2 border-black">
+      <div className="bg-white border-2 border-black min-w-[400px] h-[600px]">
         <div>매물 카드</div>
         <div>{name}</div>
       </div>
