7f92e6acb5e2b82e46306df9258bd09d405f8733
이윤서 <yyoo04@naver.com>
2025-07-24 15:24:05 +0900

    카드 컴포넌트 수정

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,7 +1,7 @@
-const PropertyCard = ({ name }) => {
+const PropertyCard = ({ name, description, price }) => {
   return (
     <>
-      <div className="bg-white border-2 border-black min-w-[400px] h-[600px]">
+      <div className="bg-white shadow-lg shadow-gray-300 min-w-[400px] h-[600px]">
         <div>매물 카드</div>
         <div>{name}</div>
       </div>
