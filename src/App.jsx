9b21bc4679c18f6065da0ccad962877731abba1a
이정인 <jung020623@g.hongik.ac.kr>
2025-07-24 16:30:52 +0900

    Fix : 카카오로그인 구현(?)

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Introduction from './pages/Introduction.jsx'
 import Announcement from './pages/Announcement.jsx'
 import MapoNews from './pages/MapoNews.jsx'
 import MyPage from './pages/MyPage.jsx'
-
+import KakaoRedirect from './pages/KakaoRedirect.jsx';
 function App() {
   return (
     <Routes>
@@ -19,6 +19,7 @@ function App() {
         <Route path="/Announcement" element={<Announcement />} />
         <Route path="/MapoNews" element={<MapoNews />} />
         <Route path="/MyPage" element={<MyPage />} />
+        <Route path="/auth/callback" element={<KakaoRedirect />} />
       </Route>
     </Routes>
   );
--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,14 +1,12 @@
 import instance from './axios';
 
 export const login = async (code) => {
-  const res = await instance.post('/login/oauth2/code/kakao', { // 추후 url 수정
-    code,
-  });
+  const res = await instance.post('/login/oauth2/code/kakao', { code });
 
   return res.data;
 };
 
 export const getCurrentUser = async () => {
-  const res = await instance.get('/......'); // 추후 url 수정
+  const res = await instance.get('/api/user/me');
   return res.data;
 };
